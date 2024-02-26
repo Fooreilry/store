bdcc65176629cb113006d7c27a3dc2b638b8e295
sergey <serdzopadzo@gmail.com>
2024-02-26 11:00:11 +0300

    update eslint config and add plugin import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'next',
     'eslint:recommended',
+    'plugin:import/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'prettier',
@@ -26,6 +27,18 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'react'],
-  rules: {},
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': [
+      'off',
+      {
+        devDependencies: false,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    '@typescript-eslint/default-param-last': 'off',
+    'import/prefer-default-export': 'off',
+  },
 };
