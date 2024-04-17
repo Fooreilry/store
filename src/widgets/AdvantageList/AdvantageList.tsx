11e7ce827a0b6161050676feb89a0bc68c3f6a4c
Lyuba <len.ovo4356@gmail.com>
2024-04-17 17:27:39 +0300

    Creating a block of advantage

---
+++ src/widgets/AdvantageList/AdvantageList.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@/src/shared/utils';
+import React from 'react';
+import { AdvantageCard } from '@/src/shared/ui/AdvantageCard/AdvantageCard';
+import { Minus } from 'lucide-react';
+import cls from './AdvantageList.module.css';
+
+export const AdvantageList = () => {
+  return (
+    <ul className={cn(cls.AdvantageList)}>
+      <li>
+        <AdvantageCard titleAdvantage="Яркий цвет" iconAdvantage={<Minus />} />
+      </li>
+      <li>
+        <AdvantageCard titleAdvantage="Разнообразие" iconAdvantage={<Minus />} />
+      </li>
+      <li>
+        <AdvantageCard titleAdvantage="Высота прыжка" iconAdvantage={<Minus />} />
+      </li>
+      <li>
+        <AdvantageCard titleAdvantage="Размеры" iconAdvantage={<Minus />} />
+      </li>
+      <li>
+        <AdvantageCard titleAdvantage="Скорость" iconAdvantage={<Minus />} />
+      </li>
+    </ul>
+  );
+};
