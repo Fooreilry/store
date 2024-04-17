2b0a4e65a491fe3cfa2606b6ef5dd2a699e84ce1
Lyuba <len.ovo4356@gmail.com>
2024-04-17 09:22:42 +0300

    fix style information block

--- src/widgets/InfoBlock/InfoBlock.tsx
+++ src/widgets/InfoBlock/InfoBlock.tsx
@@ -1,12 +1,13 @@
+import { cn } from '@/src/shared/utils';
 import styles from './InfoBlock.module.css';
 
 export const InfoBlock = () => {
   return (
-    <section className={`${styles.InfoBlockSection} min-w-80 h-[80%] bg-[url('/bg.jpg')] bg-cover`}>
-      <div className="bg-[rgb(172,124,232)] bg-opacity-[80%] min-w-80 h-[100%] grid grid-rows-2 grid-cols-4 gap-4 p-[2%]">
-        <div className={`${styles.NewsBlock} bg-red-900 mr-[2%] `} />
-        <div className={`${styles.ProductBlockTheDay} bg-cyan-600 mb-[2%]`} />
-        <div className={`${styles.BlockShares} bg-purple-950`} />
+    <section className={cn(styles.InfoBlockSection, "min-w-80 min-h-[600px] bg-[url('/bg.jpg')] bg-cover")}>
+      <div className={styles.InfoBlockDiv}>
+        <div className={cn(styles.NewsBlock, 'bg-red-900 mr-[2%]')} />
+        <div className={cn(styles.ProductBlockTheDay, 'bg-cyan-600 mb-[2%]')} />
+        <div className={cn(styles.BlockShares, 'bg-purple-950')} />
       </div>
     </section>
   );
