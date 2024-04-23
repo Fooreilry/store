22b6f66ccc7f331cc8a842062bbd65e521d81edd
Lyuba <len.ovo4356@gmail.com>
2024-04-23 11:58:55 +0300

    Creating a side menu for the admin panel

---
+++ src/widgets/SideMenu/menu.ts
@@ -0,0 +1,31 @@
+import type { LucideIcon } from 'lucide-react';
+import { CircleUser, ClipboardList, HomeIcon } from 'lucide-react';
+
+export type List = {
+  title: string;
+  path: string;
+};
+export type MenuItemType = {
+  title: string;
+  icon: LucideIcon;
+  link: string;
+};
+
+export const MENU: MenuItemType[] = [
+  {
+    title: 'Главная',
+    link: '/admin',
+    icon: HomeIcon,
+  },
+  {
+    title: 'Список товаров',
+    link: '/ListProduct',
+    icon: ClipboardList,
+  },
+
+  {
+    title: 'Пользователи',
+    link: '/user',
+    icon: CircleUser,
+  },
+];
