c16aad886013d00fc7bdb309aaa310ee758884da
Lyuba <len.ovo4356@gmail.com>
2024-04-23 13:25:01 +0300

    The main page has been added

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/src/shared/ui/button';
+
 export default function page() {
-  return <div>Admin</div>;
+  return (
+    <div className="grid grid-cols-2 w-[100%] gap-5 place-content-center justify-items-center">
+      <Button className="w-[496px] h-[134px] bg-secondary text-nune hover:bg-gray-300"> Заказы </Button>
+      <Button className="w-[496px] h-[134px] bg-secondary text-nune hover:bg-gray-300"> Товары </Button>
+      <Button className="w-[496px] h-[134px] bg-secondary text-nune hover:bg-gray-300">Пользователи</Button>
+      <Button className="w-[496px] h-[134px] bg-secondary text-nune hover:bg-gray-300"> Новости </Button>
+    </div>
+  );
 }
