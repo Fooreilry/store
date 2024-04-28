b2bbb815c2b714223dec5f998c3a2b9e75c31968
Sergey <serdzopadzo@gmail.com>
2024-04-28 22:45:24 +0300

    add filters for products

--- src/pages/AdminProducts/AdminProducts.tsx
+++ src/pages/AdminProducts/AdminProducts.tsx
@@ -2,7 +2,7 @@ import { Payment, ProductTable, columns } from '@/src/widgets/ProductTable/Produ
 
 export default function AdminProducts({ products = [] }: { products: Payment[] }) {
   return (
-    <div className=" px-10 w-full">
+    <div className=" px-10 w-full mt-8">
       <ProductTable columns={columns} data={products} />
     </div>
   );
--- src/widgets/ProductTable/ProductTable.tsx
+++ src/widgets/ProductTable/ProductTable.tsx
@@ -1,10 +1,34 @@
 'use client';
 
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 
+import { Button } from '@/src/shared/ui/button';
 import { Checkbox } from '@/src/shared/ui/checkbox';
+import { Input } from '@/src/shared/ui/input';
+import { Popover, PopoverContent, PopoverTrigger } from '@/src/shared/ui/popover';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/src/shared/ui/select';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/src/shared/ui/table';
+import { ArrowUpDown, EllipsisVertical } from 'lucide-react';
 import Image from 'next/image';
+import { useState } from 'react';
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -24,12 +48,49 @@ export type Payment = {
 };
 export const columns: ColumnDef<Payment>[] = [
   {
+    accessorKey: 'actions',
+    header: '',
+    cell: ({ row }) => {
+      return (
+        <Popover>
+          <PopoverTrigger>
+            <Button variant="ghost" size="icon" className="h-8 w-8 p-0">
+              <EllipsisVertical />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="flex flex-col items-center w-auto">
+            <Button variant="ghost" className="h-8 ">
+              Посмотреть
+            </Button>
+            <Button variant="ghost" className="h-8 text-red-600 hover:text-red-500">
+              Удалить
+            </Button>
+          </PopoverContent>
+        </Popover>
+      );
+    },
+  },
+  {
     accessorKey: 'id',
     header: 'id',
   },
   {
     accessorKey: 'name',
-    header: 'Название',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className=" text-base font-bold"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Название
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <p className=" font-bold">{row.getValue<string>('name')}</p>;
+    },
   },
   {
     accessorKey: 'img',
@@ -47,7 +108,7 @@ export const columns: ColumnDef<Payment>[] = [
   },
   {
     accessorKey: 'isActive',
-    header: 'Активен',
+    header: 'Статус',
     cell: ({ row }) => {
       return <Checkbox checked={row.getValue<boolean>('isActive')} disabled className="data-[disabled]:opacity-100 " />;
     },
@@ -58,7 +119,18 @@ export const columns: ColumnDef<Payment>[] = [
   },
   {
     accessorKey: 'createdAt',
-    header: 'Дата создания',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className=" text-base font-bold"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Дата создания
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
     cell: ({ row }) => {
       return row.getValue<Date>('createdAt').toLocaleDateString();
     },
@@ -72,7 +144,18 @@ export const columns: ColumnDef<Payment>[] = [
   },
   {
     accessorKey: 'rating',
-    header: 'Рейтинг',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className=" text-base font-bold"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Рейтинг
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
     cell: ({ row }) => {
       return row.getValue<number>('rating');
     },
@@ -80,48 +163,113 @@ export const columns: ColumnDef<Payment>[] = [
 ];
 
 export function ProductTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+
+    state: {
+      sorting,
+      columnFilters,
+    },
   });
 
+  console.log(table.getColumn('isActive'));
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead className=" text-center" key={header.id}>
-                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell className=" text-center" key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+    <>
+      <div className="flex items-center py-4 space-x-4">
+        <Input
+          placeholder="Введите название товара"
+          value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+          onChange={(event) => table.getColumn('name')?.setFilterValue(event.target.value)}
+          className="max-w-sm"
+        />
+        <Select
+          value={(table.getColumn('category')?.getFilterValue() as string) ?? undefined}
+          onValueChange={(value) => table.getColumn('category')?.setFilterValue(value)}
+        >
+          <SelectTrigger className="w-full xs:w-[240px]">
+            <SelectValue placeholder="Выберите категорию" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Категории</SelectLabel>
+              <SelectItem value="1">Category 1</SelectItem>
+              <SelectItem value="2">Category 2</SelectItem>
+              <SelectItem value="3">Category 3</SelectItem>
+              <SelectItem value="4">Category 4</SelectItem>
+              <SelectItem value="5">Category 5</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select
+          value={(table.getColumn('isActive')?.getFilterValue() as string) ?? undefined}
+          //! fix this
+          onValueChange={(value) => table.getColumn('isActive')?.setFilterValue(!!+value)}
+        >
+          <SelectTrigger className="w-full xs:w-[240px]">
+            <SelectValue placeholder="Выберите статус" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="0">Не активен</SelectItem>
+              <SelectItem value="1">Активен</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead className=" text-center text-base font-bold" key={header.id}>
+                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                Результаты не найдены
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell className=" text-center text-base" key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  Результаты не найдены
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+          Previous
+        </Button>
+        <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+          Next
+        </Button>
+      </div>
+    </>
   );
 }
